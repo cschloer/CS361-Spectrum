81a01326a168609dfa9536501b1c098c1a6116fb
abbysinger94 <abigail.singer@oberlin.edu>
2014-10-27 21:48:43 -0400

    deelies

--- Spectrum/Assets/Resources/Scripts/GameManager.js
+++ Spectrum/Assets/Resources/Scripts/GameManager.js
@@ -247,6 +247,96 @@ function OnGUI() {
 		GUI.Box(Rect(0,0,Screen.width,Screen.height), "\n\n\n\n\n\n Paused!\nHealth: "+character.health+"\nScore:"+character.killedMonsters);
 	}
 	else {
-		GUI.Label(Rect(0,0,Screen.width,Screen.height),"Health: " + character.health + "\nKilled Monsters: "+character.killedMonsters);
 	}
+	
+	var width1 = Screen.width/50;
+	var height1 = width1;
+	var boxSize = Screen.width/10;
+	
+	// Controls the jumping image of the TraitMap
+	var textJump : Texture2D;
+	textJump = Resources.Load("Textures/TraitMap_Jump", Texture2D);
+	if (character.model.blue == true){
+		GUI.color.a = 1;
+		GUI.DrawTexture(Rect(width1,height1,boxSize,boxSize), textJump, ScaleMode.ScaleToFit, true, 0);
+	} else {
+		GUI.color.a = 0.5;
+		GUI.DrawTexture(Rect(width1,height1,boxSize,boxSize), textJump, ScaleMode.ScaleToFit, true, 0);
+	}
+	
+	// -------> This is the TraitMap
+	
+	// Controls the rolling image of the TraitMap
+	var textRoll : Texture2D;
+	textRoll = Resources.Load("Textures/TraitMap_Roll", Texture2D);
+	if (character.model.blue == true){
+		GUI.color.a = 0.5;
+		GUI.DrawTexture(Rect(width1,height1*3+boxSize/2,boxSize,boxSize), textRoll, ScaleMode.ScaleToFit, true, 0);
+	} else {
+		GUI.color.a = 1;
+		GUI.DrawTexture(Rect(width1,height1*3+boxSize/2,boxSize,boxSize), textRoll, ScaleMode.ScaleToFit, true, 0);
+	}	
+	
+	// Controls the swinging image of the TraitMap
+	var textSwing : Texture2D;
+	textSwing = Resources.Load("Textures/TraitMap_Swing", Texture2D);
+	if (character.model.yellow == true){
+		GUI.color.a = 1;
+		GUI.DrawTexture(Rect(width1*3 + boxSize/2,height1,boxSize,boxSize), textSwing, ScaleMode.ScaleToFit, true, 0);
+	} else {
+		GUI.color.a = 0.5;
+		GUI.DrawTexture(Rect(width1*3 + boxSize/2,height1,boxSize,boxSize), textSwing, ScaleMode.ScaleToFit, true, 0);
+	}
+	
+	// Controls the throwing image of the TraitMap
+	var textThrow : Texture2D;
+	textThrow = Resources.Load("Textures/TraitMap_Throw", Texture2D);
+	if (character.model.yellow == true){
+		GUI.color.a = 0.5;
+		GUI.DrawTexture(Rect(width1*3 + boxSize/2,height1*3+boxSize/2,boxSize,boxSize), textThrow, ScaleMode.ScaleToFit, true, 0);
+	} else {
+		GUI.color.a = 1;
+		GUI.DrawTexture(Rect(width1*3 + boxSize/2,height1*3+boxSize/2,boxSize,boxSize), textThrow, ScaleMode.ScaleToFit, true, 0);
+	}	
+	
+	// Controls the swinging image of the TraitMap
+	var textBig : Texture2D;
+	textBig = Resources.Load("Textures/TraitMap_Big", Texture2D);
+	if (character.model.red == true){
+		GUI.color.a = 1;
+		GUI.DrawTexture(Rect(2*(width1*3 + boxSize/2),height1,boxSize,boxSize), textBig, ScaleMode.ScaleToFit, true, 0);
+	} else {
+		GUI.color.a = 0.5;
+		GUI.DrawTexture(Rect(2*(width1*3 + boxSize/2),height1,boxSize,boxSize), textBig, ScaleMode.ScaleToFit, true, 0);
+		GUI.color.a = 1;
+
+	}
+	
+	// Controls the throwing image of the TraitMap
+	var textSmall : Texture2D;
+	textSmall = Resources.Load("Textures/TraitMap_Small", Texture2D);
+	if (character.model.red == true){
+		GUI.color.a = 0.5;
+		GUI.DrawTexture(Rect(2*(width1*3 + boxSize/2),height1*3+boxSize/2,boxSize,boxSize), textSmall, ScaleMode.ScaleToFit, true, 0);
+	} else {
+		GUI.color.a = 1;
+		GUI.DrawTexture(Rect(2*(width1*3 + boxSize/2),height1*3+boxSize/2,boxSize,boxSize), textSmall, ScaleMode.ScaleToFit, true, 0);
+	}	
+	
+	// --------> This is the lifehearts
+	var textHealth : Texture2D;
+	
+	if (character.health >= 3) {
+		textHealth = Resources.Load("Textures/heart3", Texture2D);
+		GUI.DrawTexture(Rect((Screen.width/7)*5, height1, Screen.width/4, Screen.height/8), textHealth, ScaleMode.StretchToFill, true, 0);
+	}else if (character.health == 2) {
+		textHealth = Resources.Load("Textures/heart2", Texture2D);
+		GUI.DrawTexture(Rect((Screen.width/7)*5, height1, Screen.width/4, Screen.height/8), textHealth, ScaleMode.StretchToFill, true, 0);
+	}else if (character.health == 1) {
+		textHealth = Resources.Load("Textures/heart1", Texture2D);
+		GUI.DrawTexture(Rect((Screen.width/7)*5, height1, Screen.width/4, Screen.height/8), textHealth, ScaleMode.StretchToFill, true, 0);
+	}else {
+		textHealth = Resources.Load("Textures/heart0", Texture2D);
+		GUI.DrawTexture(Rect((Screen.width/7)*5, height1, Screen.width/4, Screen.height/8), textHealth, ScaleMode.StretchToFill, true, 0);
+	}				
 }
