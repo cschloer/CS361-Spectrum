b3fde6e7a4745b3900bb3bd25686f2e6a188b0bc
Hertafeld <ehertafe@oberlin.edu>
2014-11-18 00:19:17 -0500

    Improved Colliders

--- Spectrum/Assets/Resources/Scripts/CharacterModel.js
+++ Spectrum/Assets/Resources/Scripts/CharacterModel.js
@@ -1,5 +1,4 @@
-var colRect : Rect;
-var totalRect : Rect;
+
 var moveN:boolean;
 var moveW:boolean;
 var moveS:boolean;
@@ -622,24 +621,30 @@ function stopMovement(){
 }
 
 function handleCollisions(col:Collider){
-/*
+	if(rolling){
+		 heading = Vector3.zero;
+		 rjTimer = 0;
+		 rolling = false;
+	}
 	var xDist : float = transform.position.x - col.gameObject.transform.position.x;
 	var yDist : float = transform.position.y - col.gameObject.transform.position.y;
 	var xBuffer : float = (gameObject.GetComponent(BoxCollider).size.x + col.gameObject.GetComponent(BoxCollider).size.x)/2;
 	var yBuffer : float = (gameObject.GetComponent(BoxCollider).size.y + col.gameObject.GetComponent(BoxCollider).size.y)/2;
 	if((Mathf.Abs(yDist) > Mathf.Abs(xDist)) && ((heading.y > 0 && yDist < 0) || (heading.y < 0 && yDist > 0))) heading.y =0;
-	if((Mathf.Abs(xDist) > Mathf.Abs(yDist)) && ((heading.x > 0 && xDist < 0) || (heading.x < 0 && xDist > 0))) heading.x = 0;
-	*/
-	var buffer : float = .1;
+	if((Mathf.Abs(xDist) > Mathf.Abs(yDist)+.2) && ((heading.x > 0 && xDist < 0) || (heading.x < 0 && xDist > 0))) heading.x = 0;
+	
+	/*
+	var headingMod : Vector3 = 2*(heading * Time.deltaTime * moveSpeed*rollSpeedMultiplier);
+	var buffer : float = .2;
 	var boxSize : Vector3 = gameObject.GetComponent(BoxCollider).size;
-	var xRect : Rect = new Rect(transform.position.x - boxSize.x/2 + heading.x, transform.position.y - boxSize.y/2, boxSize.x, boxSize.y);
-	var yRect : Rect = new Rect(transform.position.x - boxSize.x/2, transform.position.y - boxSize.y/2 + heading.y, boxSize.x, boxSize.y);
-	var totalRect : Rect = new Rect(transform.position.x - boxSize.x/2 + heading.x, transform.position.y - boxSize.y/2 + heading.y, boxSize.x, boxSize.y);
-	var colRect : Rect = new Rect(col.gameObject.transform.position.x - col.size.x/2 - buffer, col.gameObject.transform.position.y - col.size.y/2 - buffer, col.size.x - 2*buffer ,col.size.y - 2*buffer);
+	var xRect : Rect = new Rect(transform.position.x + headingMod.x, transform.position.y, boxSize.x, boxSize.y);
+	var yRect : Rect = new Rect(transform.position.x, transform.position.y + headingMod.y, boxSize.x, boxSize.y);
+	var totalRect : Rect = new Rect(transform.position.x + headingMod.x, transform.position.y + headingMod.y, boxSize.x, boxSize.y);
+	var colRect : Rect = new Rect(col.gameObject.transform.position.x + buffer, col.gameObject.transform.position.y + buffer, col.size.x - 2*buffer ,col.size.y - 2*buffer);
 	if(xRect.Overlaps(colRect)) heading.x = 0;
 	else if(yRect.Overlaps(colRect)) heading.y = 0;
-	//else if(totalRect.Overlaps(colRect)) heading = Vector3.zero;
-	
+	else if(totalRect.Overlaps(colRect)) heading = Vector3.zero;
+	*/
 	heading.Normalize();
 } 
 
