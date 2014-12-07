02cd4d6a5c91778c8333970891d497efb57aa5d1
gfishel <gabriel@ip-91-205.wireless.oberlin.edu>
2014-12-07 13:00:23 -0500

    Camera Fixes
    The camera on level2 wasn’t working. I tried to make it the same as
    level1 and everything seems fine. Also Level three is in progress.

--- Spectrum/Assets/Resources/Scripts/GameLevel2.js
+++ Spectrum/Assets/Resources/Scripts/GameLevel2.js
@@ -13,20 +13,14 @@ function levelInit(){
   roomCreate(-10,-10,0,"Plain4End.txt");
   roomCreate(-10, 10,0,"Plain4Cross.txt");
   roomCreate(-30, 10,2,"Hole4Tri2.txt");
-  //roomCreate(-30,-10,0,"Hole2End.txt");
   roomCreate(-30, 30,2,"Walls1End.txt");
   roomCreate( 10, 10,0,"Hole4Tri.txt");
-  //roomCreate( 10,-10,0,"Plain2End.txt");
-  //roomCreate( 30, 10,3,"Plain1End.txt");
   roomCreate(-10, 30,2,"Plain4End.txt");
   addDevice(-4,25,"mSpawn", 3, 9); // magnet
   addDevice( 4,25,"mSpawn", 3, 8); // magnet
   addDevice(-4,30,"mSpawn", 4, 5); //ranged
   addDevice(-14,38,"mSpawn", 9, 7); // lots of buzz dudes
   addDevice(0,45,"mSpawn", 6, 0); // lots of buzz dudes
-  //addDevice(20,5,"mSpawn", 4, 3);
- 
-  //addDevice(1,-13,"mSpawn", 4, 5);//first one
   addDevice(-21, 25, "mSpawn", 4, 3);
   addDevice(17, 24,"mSpawn", 3, 1); //shielded in hall
   addDevice(24, 11,"mSpawn", 1, 4); //single mosnter 4
@@ -62,6 +56,17 @@ function changeColor(){
 
 function OnGUI(){
 	super.OnGUI();
+}
+
+function Update(){
+	super.Update();
+	var cams : Array = Camera.allCameras;
+	//print(cams.length);
+	for(var cam : Camera in cams){
+		cam.transform.position.x = character.model.transform.position.x;
+		cam.transform.position.y = character.model.transform.position.y;
+		if (cam != Camera.main) cam.transform.position.z = -20;
+		}
 }
 
 
---
+++ Spectrum/Assets/Resources/Scripts/GameLevel3.js
@@ -0,0 +1,56 @@
+﻿
+
+public class GameLevel3 extends GameManager {
+
+
+function Start(){
+	super.Start();
+	musicSound.clip = Resources.Load("Sounds/rhysGameMusic");
+	musicSound.Play();
+}
+
+function levelInit(){
+  roomCreate(-10,-10,0,"Plain5Tri.txt");
+  roomCreate(-30, -10,0,"Plain2Adj.txt"); // Monster Left
+  roomCreate(-30, 10,0,"Plain2Opp.txt");  // Monster Left
+  roomCreate(-30, 30,0,"Plain4Opp.txt");  // Monter Left
+  roomCreate( 10,-10,3,"Hole2Adj.txt");   // Hole Right
+  roomCreate( 10, 10,0,"Hole5Tri.txt");   // Hole Right
+  addCake(-0.5,15);
+  addCake(-0.5,0);
+  addCake(-1.5,40);
+  addCake(-22,16);
+  addCake(4.5,7.5);
+  addCake(13,12);
+  addCake(-17,28);
+  addCake(8,48);
+  charSpawner = addDevice(-0.5,-5,"aSpawn",0,0);
+  bossSpawner = addDevice(-21, 40,"aSpawn",1,0);
+  charSpawner.modelObject.GetComponent("SpawnPointModel").spawn();
+  bossSpawner.modelObject.GetComponent("SpawnPointModel").spawn();
+  currentLevel=6;
+}
+
+
+function addBoss(x : float, y :float, c : Character){
+	//todo: Rewire for new boss.
+	addBossTentacle(x,y,c);
+}
+
+function OnGUI(){
+	super.OnGUI();
+}
+
+function Update(){
+	super.Update();
+	var cams : Array = Camera.allCameras;
+	//print(cams.length);
+	for(var cam : Camera in cams){
+		cam.transform.position.x = character.model.transform.position.x;
+		cam.transform.position.y = character.model.transform.position.y;
+		if (cam != Camera.main) cam.transform.position.z = -20;
+		}
+}
+
+
+}
