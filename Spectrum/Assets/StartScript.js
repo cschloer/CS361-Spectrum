e6c874beb5a2c59318556fa396608f57de97f686
abbysinger94 <abigail.singer@oberlin.edu>
2014-12-09 09:54:22 -0500

    start buttonz

--- Spectrum/Assets/StartScript.js
+++ Spectrum/Assets/StartScript.js
@@ -5,6 +5,9 @@ var exitButton : Texture2D;
 var levelButton : Texture2D;
 var levelName : String;
 var levelNum : int;
+var guif : Font;
+var SpectrumSkin : GUISkin;
+
 
 function Start () {
 	startButton = Resources.Load("Textures/StartButton", Texture2D);
@@ -18,6 +21,9 @@ function Start () {
 	}else{
 		levelName = "Level"+(levelNum-3);
 	}
+		guif = Resources.Load("GUI_Components/Arabolic", Font) as Font;
+	SpectrumSkin = Resources.Load("GUI_Components/SpectrumSkin", GUISkin) as GUISkin;
+
 }
 
 function Update () {
@@ -25,15 +31,27 @@ function Update () {
 }
 
 function OnGUI() {
+
+	GUI.backgroundColor = Color.white;
+	GUI.skin.label.fontSize = 30;
+	GUI.skin = SpectrumSkin;
+	GUI.skin.font = guif;
+	GUI.skin.box.fontSize = 30;
+
+
+	
 	GUI.depth = 3;
 	if (GUI.Button (Rect((Screen.width/10)*1, (Screen.height/7)*5, Screen.width/2.5, Screen.height/6), startButton)) {
         Application.LoadLevel(levelName);
     }
-    
+    	GUI.Box (Rect((Screen.width/10)*1, (Screen.height/7)*5, Screen.width/2.5, Screen.height/6), "Start"); 
+
     if (GUI.Button (Rect((Screen.width/9)*5, (Screen.height/7)*5, Screen.width/3, Screen.height/6), exitButton)) {
         Application.Quit();
     }
     
+      GUI.Box (Rect((Screen.width/9)*5, (Screen.height/7)*5, Screen.width/3, Screen.height/6), "Quit"); 
+
     if (GUI.Button (Rect((Screen.width/10)*9, (Screen.height/10)*9, Screen.width/10, Screen.height/8), levelButton)) {
         Application.LoadLevel("LevelEditor");
     }
